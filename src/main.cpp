2c3fce15d380354d05539fc0b0b19387a47bffb1
luping42 <levi.spiess@bluewin.ch>
2025-03-07 10:29:47 +0100

    Servos

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 // drivers
 #include "DebounceIn.h"
 
+// servo
+#include "Servo.h"
+
+
 bool do_execute_main_task = false; // this variable will be toggled via the user button (blue button) and
                                    // decides whether to execute the main task or not
 bool do_reset_all_once = false;    // this variable is used to reset certain variables and objects and
@@ -37,6 +41,36 @@ int main()
     // a led has an anode (+) and a cathode (-), the cathode needs to be connected to ground via the resistor
     DigitalOut led1(PB_9);
 
+    // servo
+        Servo servo_D0(PB_D0);
+        Servo servo_D1(PB_D1);
+        float servo_input = 0.0f;
+
+
+        // minimal pulse width and maximal pulse width obtained from the servo calibration process
+        // futuba S3001
+        float servo_D0_ang_min = 0.0150f; // carefull, these values might differ from servo to servo
+        float servo_D0_ang_max = 0.1150f;
+        // reely S0090
+        float servo_D1_ang_min = 0.0325f;
+        float servo_D1_ang_max = 0.1175f;
+
+        // servo.setPulseWidth: before calibration (0,1) -> (min pwm, max pwm)
+        // servo.setPulseWidth: after calibration (0,1) -> (servo_D0_ang_min, servo_D0_ang_max)
+        servo_D0.calibratePulseMinMax(servo_D0_ang_min, servo_D0_ang_max);
+        servo_D1.calibratePulseMinMax(servo_D1_ang_min, servo_D1_ang_max);
+
+        // default acceleration of the servo motion profile is 1.0e6f
+        servo_D0.setMaxAcceleration(0.3f);
+        servo_D1.setMaxAcceleration(0.3f);
+
+        int servo_counter = 0; // define servo counter, this is an additional variable
+        // used to command the servo
+        const int loops_per_seconds = static_cast<int>(ceilf(1.0f / (0.001f * static_cast<float>(main_task_period_ms))));
+
+
+
+
     // start timer
     main_task_timer.start();
 
@@ -44,19 +78,48 @@ int main()
     while (true) {
         main_task_timer.reset();
 
+        
+
         if (do_execute_main_task) {
 
-            // visual feedback that the main task is executed, setting this once would actually be enough
-            led1 = 1;
-        } else {
-            // the following code block gets executed only once
-            if (do_reset_all_once) {
-                do_reset_all_once = false;
+        
 
-                // reset variables and objects
-                led1 = 0;
-            }
+        // enable the servos
+        if (!servo_D0.isEnabled())
+            servo_D0.enable();
+        if (!servo_D1.isEnabled())
+            servo_D1.enable();
+
+        // command the servos
+        servo_D0.setPulseWidth(servo_input);
+        servo_D1.setPulseWidth(servo_input);
+
+        // calculate inputs for the servos for the next cycle
+        if ((servo_input < 1.0f) &&                     // constrain servo_input to be < 1.0f
+            (servo_counter % loops_per_seconds == 0) && // true if servo_counter is a multiple of loops_per_second
+            (servo_counter != 0))                       // avoid servo_counter = 0
+            servo_input += 0.005f;
+        servo_counter++;
+
+
+
+        // print to the serial terminal
+        printf("Pulse width: %f \n", servo_input);
+
+
+        } else {
+
+        // reset variables and objects
+        led1 = 0;
+        servo_D0.disable();
+        servo_D1.disable();
+        servo_input = 0.0f;
+ 
         }
+
+        
+
+
 
         // toggling the user led
         user_led = !user_led;
